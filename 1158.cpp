2329bbae9dcace62b7e882638152e1a76a8d7e46
ikjun jang <rtxqzdt27@gmail.com>
2023-12-18 13:45:24 +0000

    1158

---
+++ 1158.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <stdlib.h>
+
+
+using namespace std;
+
+ 
+
+
+int main(void) {
+    
+    int n;
+    int k;
+    
+
+    cin >> n >> k;
+
+    
+
+
+    return 0;
+}
