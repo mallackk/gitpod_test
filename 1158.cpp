0c897b08020d8b799f5ad48069a768110b70969c
ikjun jang <rtxqzdt27@gmail.com>
2023-12-19 14:45:13 +0000

    1158 2nd try

--- 1158.cpp
+++ 1158.cpp
@@ -14,9 +14,24 @@ int main(void) {
     
     int n;
     int k;
-    
 
     cin >> n >> k;
+
+    list<int> Y;
+    list<int>::iterator iter=Y.begin();
+
+    for(int i=0; i < n; i++) {
+        Y.push_back(i);
+    }
+
+    while(Y.size()!=0;) {
+        for(int i=0; i < k-1; i++) {
+            ++iter;
+            Y.erase(iter);
+            --iter;
+        }
+        if
+    }
 
     
 
