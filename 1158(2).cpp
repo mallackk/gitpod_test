822b77b316f4444ad03bfb99a1ba8217517035e0
ikjun jang <rtxqzdt27@gmail.com>
2023-12-21 13:30:50 +0000

    1158 3rd try

--- 1158(2).cpp
+++ 1158(2).cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(void) {
-    ios::sync_with_stdio(0);
+    ios::sync_with_stdio(false);
     cin.tie(0);
     
     int n, k;
@@ -12,28 +12,36 @@ int main(void) {
     cin >> n >> k;
 
     list<int> Y;
-    list<int>::iterator iter = Y.begin();
-    for(int i=1; i<=n; i++) {
+    list<int>::iterator kill = Y.begin();
+
+    for(int i=1; i <= n; i++) {
         Y.push_back(i);
     }
-    
     cout << "<";
 
     while(Y.size()) {
-        for(int i=0; i < k; i++) {
-            if(++iter == Y.end()) {
-                iter = Y.begin();
+        for(int i=0; i < k-1; i++) {
+            if(kill == Y.end()) {
+                kill = Y.begin();
                 continue;
             }
-            ++iter;
-        }
-        cout << *iter;
-        iter = --Y.erase(iter);
-
-        if(Y.empty())
-            cout << ">";
+            ++kill;
+        } // 여기까지가 이제 죽일사람 정하는거
+        if(Y.size() == 1)
+            cout << *kill;
         else
-            cout << ", ";
+            cout << *kill << ", ";
+    
+        kill = Y.erase(kill);
+        if(kill == Y.end()) {
+            kill = Y.begin();
+        }
+        //이제 죽여야지
     }
+
+
+    cout << ">";
     return 0;
 }
+
+//왜안될까..?
