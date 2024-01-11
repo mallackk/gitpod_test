f879c7e12017a8b1a6d56dbd5ae62d30da51b46b
ikjun jang <rtxqzdt27@gmail.com>
2024-01-11 13:32:22 +0000

    2798 complete

--- 2798.cpp
+++ 2798.cpp
@@ -3,35 +3,43 @@
 
 using namespace std;
 
+int card[100];
+
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int card[101];
-    int n, m, ans;
- 
+    int n;
+    int m;
+    int tmp;
+    int max = 0;
     cin >> n >> m;
+
     for(int i=0; i < n; i++) {
-        cin >> card[i]; //100,000을 넘지 않는 양의 정수 조건은?
-    }
+        cin >> card[i];
+    }    
 
     for(int i=0; i < n; i++) {
         for(int j=i+1; j < n; j++) {
             for(int k=j+1; k < n; k++) {
-                int tmp = card[i] + card[j] + card[k];
-                if(tmp <= m)
-                    ans = max(ans, tmp);                    
+                tmp = card[i] + card[j] + card[k];
+                if(max <= tmp && tmp <= m)
+                    max = tmp;
             }
         }
     }
-    cout << ans << '\n';
+
+    cout << max << endl;
+
     return 0;
 }
-
 
 // 고려해야할 점
 // 합이 M을 넘지 않는 카드 3장을 찾을 수 있는 경우에만
 // 입력으로 주어진다는건 조건을 걸어야한다는 것.
 // n개의 카드 중에서 m과 가장 가까운 세장의 합을 만드는 것...
 // for문 다시 공부해보자...
+
+
+// 원래 tmp와 새로운 tmp를 비교해야한다.
