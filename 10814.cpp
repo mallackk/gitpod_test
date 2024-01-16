cc846a526c2f13f28ca0e96d2b03691953528b45
ikjun jang <rtxqzdt27@gmail.com>
2024-01-16 14:05:17 +0000

    10814

---
+++ 10814.cpp
@@ -0,0 +1,25 @@
+// 조건: 나이 순, 가입 순
+// 그럼 tuple을 쓰는 경우가 있고
+// pair와 조건 하나를 더 붙이는 경우가 있다.
+// sort()에 대하여 공부해보자.
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Person {
+    int age, index;
+    string name;
+};
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+    
+    
+}
