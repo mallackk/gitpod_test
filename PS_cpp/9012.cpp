3d673af7762265f9b941ac410fbd669104831f68
ikjun jang <rtxqzdt27@gmail.com>
2024-01-27 23:45:46 +0000

    2164 trying

--- PS_cpp/9012.cpp
+++ PS_cpp/9012.cpp
@@ -5,7 +5,7 @@
 // ) 가 이전에 ( 이 나온 만큼보다 더 나오면 그건 잘못된 표기법임.
 // 아하!
 // 그럼 stack의 push를 (로 두고 )를 pop으로.
-#include <iostream>
+/*#include <iostream>
 #include <stack>
 #include <string>
 
@@ -45,6 +45,39 @@ int main(void) {
     
     }
     return 0;
+}*/
+
+// 다른 방법은 없을까.
+// 내생각엔 처음에 내가 생각한 부분에서 오류가 있었다.
+// 무조건 반례를 찾아서 조건을 정확하게 만들어야 한다.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    int num;
+    cin >> num;
+
+    while(num--) {
+        int cnt = 0;
+        string s;
+        cin >> s;
+
+        for(auto c : s) {
+            if(c == '(') 
+              cnt++;              
+            if(c == ')') 
+                cnt--;            
+            if(cnt < 0) {
+                break;
+            }
+        }
+        if(cnt == 0)
+            cout << "YES" << "\n";
+        else
+            cout << "NO" << "\n";
+    }
+    return 0;
 }
-
-
