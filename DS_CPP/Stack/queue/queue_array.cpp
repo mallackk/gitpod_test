4809eb9a1ffead4e1b010952c7ee1eb8da73f6bd
ikjun jang <rtxqzdt27@gmail.com>
2024-01-29 14:26:28 +0000

    1253 1st try

--- DS_CPP/Stack/queue/queue_array.cpp
+++ DS_CPP/Stack/queue/queue_array.cpp
@@ -1,1 +1,12 @@
+// enqueue(삽입), dequeue(삭제)
+// 먼저 배열로 구현해보자.
+// enqueue : O(1)
+// dequeue : O(N)
 #include <iostream>
+
+using namespace std;
+
+int arr[1000];
+
+
+
---
+++ PS_cpp/1253.cpp
@@ -0,0 +1,30 @@
+// 1 2 3 4 5 6 7 8 9 10
+// 5 10 15 25 40 100
+// 2
+
+// 어렵다...
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+    int arr[2000];    
+    for(int i=0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    
+
+
+    return 0;
+}
